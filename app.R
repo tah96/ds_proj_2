4bfe2460ff9b6f50bd215ae49b9cbdadebd8f23f
Tyler Hunt <tylerandrewhunt96@gmail.com>
2024-10-31 21:09:07 -0400

    Numeric variable sliders or range added

--- app.R
+++ app.R
@@ -3,20 +3,32 @@ library(shinyalert)
 library(tidyverse)
 source("helpers.R")
 
-housing_data <- read_csv(file="data/Melbourne_Housing_FULL.csv",show_col_types = FALSE)
+#housing_data <- read_csv(file="data/Melbourne_Housing_FULL.csv",show_col_types = FALSE)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   titlePanel("Melbourne Housing Project"),
   sidebarLayout(
     sidebarPanel(
-      h2("Input Parameters"),
+      h3("Input Parameters"),
       selectInput("region", label = "Region", 
                   choices = region_vector, 
-                  selected = region_vector[1]),
-      dateRangeInput("dateOfSale", label = "Date Sold", start = min(housing_data$Date), end = max(housing_data$Date), 
-                     min = min(housing_data$Date),
-                     max = max(housing_data$Date), format = "mm-dd-yyyy")
+                  selected = "All"),
+      checkboxGroupInput("type", label = "Housing Type", 
+                         choices = c("All",type_vector),
+                         selected = "All"),
+      ### Need to figure date out
+      dateRangeInput('saleDate',
+                     label = 'Selling Date Range',
+                     start = min(housing_data$Date), end = max(housing_data$Date),
+                     min = min(housing_data$Date), max = max(housing_data$Date),
+                     separator = " to ", format = "mm-dd-yyyy"
+      ),
+      sliderInput( 
+        "price", "Price (Thousands AUD)", 
+        min = min(housing_data$Price)/1000, max = max(housing_data$Price)/1000, 
+        value = c(350, 2000) 
+      )
     ),
     mainPanel()
   )
--- helpers.R
+++ helpers.R
@@ -1,5 +1,14 @@
 library(tidyverse)
+library(lubridate)
 
-housing_data <- read_csv(file="data/Melbourne_Housing_FULL.csv",show_col_types = FALSE)
+housing_data <- read_csv(file="data/Melbourne_Housing_FULL.csv",show_col_types = FALSE) %>%
+  drop_na(Regionname,Type,Price,Date) %>%
+  mutate(
+    Date = dmy(Date)
+  )
 
-region_vector <- sort(unique(housing_data$Regionname[which(housing_data$Regionname != "#N/A")]))
+region_vector <- c("All",sort(unique(housing_data$Regionname[which(housing_data$Regionname != "#N/A")])))
+
+type_vector <- c("House" = "h",
+                 "Townhome" = "t",
+                 "Unit/Apt" = "u")
